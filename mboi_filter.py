8705a31372d1f0954547c27919dc296613f64bed
ChengLiLab <himlate0325@gmail.com>
2018-03-30 16:40:49 +0800

    Update mboi_filter.py

--- mboi_filter.py
+++ mboi_filter.py
@@ -5,20 +5,20 @@ import sys
 args=sys.argv
 infilename=args[args.index('-i')+1]
 outfilename=args[args.index('-o')+1]
-genome = open('/lustre/user/liclab/jialm/resource/hg19.filter2.txt','r') # 24 条染色体序列信息，每条一行
+genome = open('/lustre/user/liclab/jialm/resource/hg19.filter2.txt','r') # sequences of 24 chromosomes, each a line 
 reads = open(infilename,'r')
 result = open(outfilename,'w')
 
 data=[]
 for chrom in genome:
         a = chrom.strip().upper()
-        data.append(a) # 生成基因组信息的list列表
+        data.append(a) 
 
-#reads信息： 1.read名，2正负链（0/1），3 chr(1~24), 4 起始位置        
+#reads info： 1.read name，2.strand，3 chr(1~24), 4 start sites        
 for read in reads:
-        n = int(read.split(' ')[3]) # n: read 起始位置
+        n = int(read.split(' ')[3]) # n: read start sites
         
-        if read.split(' ')[1] == '0': # read 在正链 
+        if read.split(' ')[1] == '0': # strand + 
             f = data[int(read.split(' ')[2])-1][n:(n+536)] # all the alignment start location locates at the 5' of the positive chain
             if f.find('GATC') != -1:
                     result.write(read)
