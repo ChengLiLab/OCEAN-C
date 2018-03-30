e1b2fe0919b2f8cf39ab9122e0e6be37e6844344
ChengLiLab <himlate0325@gmail.com>
2018-03-30 16:23:45 +0800

    Update bwa_mapping.sh

--- bwa_mapping.sh
+++ bwa_mapping.sh
@@ -4,6 +4,6 @@ do
     RAWNAME=${BASE%%.*}
     DIR=`dirname $line`
     mkdir -p "$DIR/BAM"
-    echo "bowtie2 -p 8 --phred33 -x /lustre/user/liclab/publicData/reference/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome $line -S $DIR/BAM/$RAWNAME.trim.sam &"
+    echo "bowtie2 -p 8 --phred33 -x ~/user/Bowtie2Index/genome $line -S $DIR/BAM/$RAWNAME.trim.sam &"
 done < $1
 
