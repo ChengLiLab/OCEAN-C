a0f60dac47ff520f8e6eea8df7cb27470272c8fd
ChengLiLab <himlate0325@gmail.com>
2018-03-30 17:36:12 +0800

    Update sort_bam_by_reads_name.sh

--- sort_bam_by_reads_name.sh
+++ sort_bam_by_reads_name.sh
@@ -4,5 +4,5 @@ do
     BASE=`basename $line`
     RAWNAME=${BASE%%.*}
     DIR=`dirname $line`
-    samtools sort  -m 6000000000 -n $line $DIR/$RAWNAME.trim.merge.unique.sort.rmd.namesorted.bam &
+    samtools sort  -m 6000000000 -n $line $DIR/$RAWNAME.trim.unique.sort.rmd.namesorted.bam &
 done < $1
