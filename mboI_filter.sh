d099d8c7988eff5d9509b872c7124a5fd52bf64a
ChengLiLab <himlate0325@gmail.com>
2018-03-30 19:07:17 +0800

    Update mboI_filter.sh

--- mboI_filter.sh
+++ mboI_filter.sh
@@ -4,7 +4,7 @@ do
     BASE=`basename $line`
     RAWNAME=${BASE%%.*}
     DIR=`dirname $line`
-    python ~/mboi_filter.py -i $line -o $DIR/$RAWNAME.trim.merge.unique.sort.rmd.namesorted.chromatin_filter.mboi_filter.txt &
+    python ~/mboi_filter.py -i $line -o $DIR/$RAWNAME.trim.unique.sort.rmd.namesorted.chromatin_filter.mboi_filter.txt &
 
 done < $1
 
