885fb352ae410bcf944af1713e23a51d43c56ce8
ChengLiLab <himlate0325@gmail.com>
2018-03-30 17:30:08 +0800

    Update remove_dup_picard.sh

--- remove_dup_picard.sh
+++ remove_dup_picard.sh
@@ -3,6 +3,6 @@ do
 BASE=`basename $line`
 RAWNAME=${BASE%%.*}
 DIR=`dirname $line`
-echo "java -Xms2g -Xmx8g -XX:ParallelGCThreads=8 -jar ~/picard-tools-1.140/picard.jar MarkDuplicates INPUT=$line OUTPUT=$DIR/$RAWNAME.trim.merge.unique.sort.rmd.bam METRICS_FILE=$DIR/$RAWNAME.trim.merge.unique.rmd.sort.bam.metrics ASSUME_SORTED=true REMOVE_DUPLICATES=true > $DIR/$RAWNAME.trim.merge.unique.sort.rmd.log &"
+echo "java -Xms2g -Xmx8g -XX:ParallelGCThreads=8 -jar ~/picard-tools-1.140/picard.jar MarkDuplicates INPUT=$line OUTPUT=$DIR/$RAWNAME.trim.unique.sort.rmd.bam METRICS_FILE=$DIR/$RAWNAME.trim.merge.unique.rmd.sort.bam.metrics ASSUME_SORTED=true REMOVE_DUPLICATES=true > $DIR/$RAWNAME.trim.merge.unique.sort.rmd.log &"
 done < $1
 
