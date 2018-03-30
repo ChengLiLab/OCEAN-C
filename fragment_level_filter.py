460ab7f4a98b19b81015053dde6ddc129bcdd8e7
ChengLiLab <himlate0325@gmail.com>
2018-03-30 16:36:41 +0800

    Update fragment_level_filter.py

--- fragment_level_filter.py
+++ fragment_level_filter.py
@@ -6,13 +6,13 @@ outfilename=args[args.index('-o')+1]
 reads = open(infilename,'r')
 results = open(outfilename,'w')
 for read in reads:
-    if read.split('\t')[7] == "3":
+    if read.split(' ')[7] == "3":
         results.write(read)
     else:
-        if read.split('\t')[7] == '1':
-            if int(read.split('\t')[8]) > 1000:
+        if read.split(' ')[7] == '1':
+            if int(read.split(' ')[8]) > 1000:
                 results.write(read)
         else:
-            if int(read.split('\t')[8]) > 1000:
+            if int(read.split(' ')[8]) > 25000:
                 results.write(read)
 results.close()
