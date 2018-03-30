9ce654b8906b57f48fca98f835d714c11630a50a
ChengLiLab <himlate0325@gmail.com>
2018-03-30 16:41:39 +0800

    Delete merge_bam_files.sh

--- merge_bam_files.sh
+++
@@ -1,5 +0,0 @@
-samtools merge /lustre/user/liclab/jialm/faire-seq/hicdata/ourdatahiC_hela/hela_hic/fastq/BAM/hela-hic-1_DO15122852_HJ535CCXX.trim.merge.bam  /lustre/user/liclab/jialm/faire-seq/hicdata/ourdatahiC_hela/hela_hic/fastq/BAM/*.trim.bam &
-
-samtools merge /lustre/user/liclab/jialm/faire-seq/faireCdata/ourdatafaireC_hela/faire_c_hela_20151231/fastq/hela_faire_c_1/BAM/hela-faire-c-1_DO15122852_HJ535CCXX.trim.merge.bam  /lustre/user/liclab/jialm/faire-seq/faireCdata/ourdatafaireC_hela/faire_c_hela_20151231/fastq/hela_faire_c_1/BAM/*.bam &
-
-samtools merge /lustre/user/liclab/jialm/faire-seq/faireCdata/ourdatafaireC_hela/faire_c_hela_20151231/fastq/hela_faire_c_2/BAM/hela-faire-c-2_DO15122852_HJ535CCXX.trim.merge.bam  /lustre/user/liclab/jialm/faire-seq/faireCdata/ourdatafaireC_hela/faire_c_hela_20151231/fastq/hela_faire_c_2/BAM/*.bam &
