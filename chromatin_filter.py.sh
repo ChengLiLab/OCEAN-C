eb6f028c66ccea7e1d56f32707e71f867cf8c338
ChengLiLab <himlate0325@gmail.com>
2018-03-30 16:25:38 +0800

    Update chromatin_filter.py.sh

--- chromatin_filter.py.sh
+++ chromatin_filter.py.sh
@@ -4,6 +4,6 @@ do
     BASE=`basename $line`
     RAWNAME=${BASE%%.*}
     DIR=`dirname $line`
-    python /lustre/user/liclab/jialm/CODE/chromatin_filter.py -i $line -o $DIR/$RAWNAME.trim.merge.unique.sort.rmd.namesorted.chromatin_filter.txt &
+    python ~/user/CODE/chromatin_filter.py -i $line -o $DIR/$RAWNAME.trim.merge.unique.sort.rmd.namesorted.chromatin_filter.txt &
 
 done < $1
